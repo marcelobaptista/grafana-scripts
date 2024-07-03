74d377f5a8930fbe21c695ad1bea02ecf34a7e16
Marcelo Baptista <marcelobaptista@outlook.com>
2024-07-02 22:02:34 -0300

    mudança na estrutura

--- list-dashboards.sh
+++ list-dashboards.sh
@@ -4,7 +4,7 @@
 # Script para listar todos os dashboards do Grafana e gerar um CSV #
 #####################################################################
 
-# Este script solicita a URL do Grafana, um token de acesso à API do Grafana 
+# Este script solicita a URL do Grafana, um token de acesso à API do Grafana
 # e o nome do arquivo de saída. Em seguida, ele lista todos os dashboards disponíveis
 # no Grafana e os salva em um arquivo CSV com as seguintes colunas:
 # Folder;Dashboard;Version;createdBy;Created;UpdatedBy;Updated;URL
@@ -12,72 +12,38 @@
 # Habilita o modo de saída de erro
 set -euo pipefail
 
-# Função para exibir mensagem de erro e sair
-exit_with_error() {
-    echo "Erro: $1"
+# Verifica se o arquivo a ser criado foi passado como argumento
+if [ $# -ne 1 ]; then
+    echo "Uso do script: $0 nome_do_arquivo_a_ser_criado"
     exit 1
-}
+fi
 
-# Função para testar o acesso à API do Grafana
-grafana_check_api() {
-    check_api=$(curl -sk -X GET "${grafana_url}/api/org" \
+# Remove o arquivo se ele existir
+if [ -f "$1.csv" ]; then
+  rm -f "$1.csv"
+  echo "Arquivo removido: $1.csv"
+fi
+
+token="" # Token de acesso ao Grafana
+grafana_url="" # URL do Grafana
+
+# Consulta a API do Grafana para obter a lista de dashboards e salva os UIDs em um arquivo
+curl -sk "${grafana_url}/api/search?type=dash-db&limit=5000" \
+    -H "Accept: application/json" \
+    -H "Authorization: Bearer ${token}" \
+    -H "Content-Type: application/json" |
+    jq -r '.[] | .uid' >dashboards_uid.txt
+
+while IFS= read -r uid; do
+    # Consulta a API para obter informações sobre o dashboard e salva em um arquivo JSON
+    curl -sk "${grafana_url}/api/dashboards/uid/${uid}" \
         -H "Accept: application/json" \
-        -H "Content-Type: application/json" \
-        -H "Authorization: Bearer ${token}")
-    if [[ -z "${check_api}" ]]; then
-        exit_with_error "Verifique a URL fornecida"
-    fi
-    if [[ "${check_api}" == *"Invalid API key"* ]]; then
-        exit_with_error "Token inválido"
-    fi
-}
-
-# Função para fazer a solicitação à API Grafana
-grafana_api_request() {
-    local url="$1"
-    local token="$2"
-    curl -sk -H "Authorization: Bearer ${token}" "${url}"
-}
-
-# Função para listar todos os dashboards
-list_dashboards() {
-    grafana_api_request "${grafana_url}/api/search?type=dash-db&limit=5000" "${token}" |
-        jq -r '.[].uid' >dashboards_uid.txt
-    while IFS= read -r uid; do
-        grafana_api_request "${grafana_url}/api/dashboards/uid/${uid}" "${token}" |
-            jq -r '. |
-"\(.meta.folderTitle);\(.dashboard.title);\(.meta.version);\(.meta.createdBy);\(.meta.created);\(.meta.updatedBy);\(.meta.updated);'"${grafana_url}"'\(.meta.url)"'
-    done <"dashboards_uid.txt" >"${file}.csv"
-    sed -i "1s/^/Folder;Dashboard;Version;createdBy;Created;UpdatedBy;Updated;URL\n/" "${file}.csv"
-    rm dashboards_uid.txt
-    printf "Arquivo %s${file}.csv gerado com sucesso: \n"
-}
-
-# Solicita a URL do Grafana
-printf "Digite a URL do Grafana (ex: http://127.0.0.1:3000)\n\n"
-read -r grafana_url
-[[ -z "${grafana_url}" ]] && exit_with_error "URL do Grafana não pode ser vazia"
-
-# Solicita o token de acesso à API do Grafana
-printf "\nDigite o token:\n\n"
-read -r token
-[[ -z "${token}" ]] && exit_with_error "Token não pode ser vazio"
-
-# Solicita o nome do arquivo de saída, sem extensão
-printf "\nDigite o nome do arquivo de saída, sem extensão\n\n"
-read -r file
-[[ -z "${file}" ]] && exit_with_error "Nome do arquivo não pode ser vazio"
-
-clear
-
-# Desabilita o modo de saída de erro
-set +e
-
-# Verifica se a URL e o token são válidos
-grafana_check_api
-
-# Habilita o modo de saída de erro novamente
-set -e
-
-# Lista os dashboards
-list_dashboards
+        -H "Authorization: Bearer ${token}" \
+        -H "Content-Type: application/json" |
+        jq -r '
+. |
+"\(.meta.folderTitle);\(.dashboard.title);\(.meta.version);\(.meta.createdBy);\(.meta.created);\(.meta.updated);\(.meta.updatedBy);'"${grafana_url}"'\(.meta.url)"
+' >>"$1.csv"
+done <"dashboards_uid.txt"
+sed -i "1s/^/Folder;Dashboard;Version;createdBy;Created;Updated;UpdatedBy;URL\n/" "$1.csv"
+rm dashboards_uid.txt
